16338f4ef294413b9913d6f8bff5d43864e0ea6f
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-24 02:15:12 +0530

    Assignment on Traffic Signal Enum

---
+++ TrafficSignalEnum/TrafficSignalEnum/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace TrafficSignalEnum
+{
+    internal class Program
+    {
+        public enum TrafficLight
+        {
+            RED,
+            YELLOW,
+            GREEN
+        }
+
+        public static void Action(TrafficLight signal)
+        {
+           
+            if(signal == TrafficLight.RED)
+            {
+                Console.WriteLine("Signal: RED -> Stop");
+            } else if(signal == TrafficLight.YELLOW)
+            {
+                Console.WriteLine("Signal: YELLOW -> Get Ready");
+            } else if (signal == TrafficLight.GREEN)
+            {
+                Console.WriteLine("Signal: GREEN -> Go");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Signal");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Traffic Signal");
+            Console.WriteLine("RED \nYELLOW \nGREEN");
+            Console.Write("Enter your choice: ");
+            string signal = Console.ReadLine();
+
+            bool isValid = Enum.TryParse<TrafficLight>(signal, true, out TrafficLight light);
+
+            if (isValid)
+            {
+                Action(light);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Signal");
+            }
+
+        }
+    }
+}
